2af09942b02862afd24b0a35c0a33ddacc8c20bf
Atharv <atharvbhandare2001@gmail.com>
2022-03-02 18:47:57 +0530

    average pair

---
+++ Problem solving patterns/Multiple Pointers/averagePair.js
@@ -0,0 +1,16 @@
+function averagePair(arr, num) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let avg = (arr[left] + arr[right]) / 2;
+    if (avg === num) {
+      return true;
+    } else if (avg < num) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return false;
+}
+averagePair([1, 2, 3], 2.5);
