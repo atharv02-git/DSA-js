4a6bd85733db62619e24b0f45c54574142609aac
Atharv <atharvbhandare2001@gmail.com>
2022-03-03 16:47:30 +0530

    absolute Recursion

---
+++ Recursion/factorial.js
@@ -0,0 +1,16 @@
+//writing factorial Iteratively
+/*function fact(num){
+    let total = 1;
+    for(let i = num; i > 1; i--){
+        total *= i;
+    }
+    return total;
+}
+console.log(fact(4));*/
+
+// Writing function for factorial recursiely
+function factorial(num){
+    if(num === 1) return 1;
+    return num * factorial(num-1)
+}
+console.log(factorial(4));
---
+++ Recursion/oddValuesCollection.js
@@ -0,0 +1,14 @@
+// collecting odd values by concatenating
+function collectOddValues(arr) {
+  let newArr = [];
+  if (arr.length === 0) {
+    return newArrṇ;
+  }
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+  return newArr;
+}
+console.log(collectOddValues([1, 2, 3, 4, 5]));
