85378a464eb46a5aa0c6dd1df462d8f95b23c927
Atharv <atharvbhandare2001@gmail.com>
2022-03-08 17:19:16 +0530

    Bubble Sort unoptimized version

---
+++ Sorting Algorithms/BubbleSort_unOptimized_version.js
@@ -0,0 +1,16 @@
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        // swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubbleSort([37, 45, 29, 8, 12, 88, -3]));
