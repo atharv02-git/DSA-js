cdcd5b98006cd14c272b5ec62906ae9c422ea684
Atharv <atharvbhandare2001@gmail.com>
2022-03-08 17:28:33 +0530

    Bubble Sort unoptimized version: Adding noSwaps to save unnecessary looping through

--- Sorting Algorithms/BubbleSort_unOptimized_version.js
+++ Sorting Algorithms/BubbleSort_unOptimized_version.js
@@ -1,6 +1,8 @@
 // UNOPTIMIZED VERSION OF BUBBLE SORT
 function bubbleSort(arr) {
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
@@ -8,8 +10,10 @@ function bubbleSort(arr) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
