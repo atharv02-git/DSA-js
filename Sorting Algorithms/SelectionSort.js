4279e987d83d56e3783c7db9ca75a353463d8dee
Atharv <atharvbhandare2001@gmail.com>
2022-03-08 19:00:36 +0530

    Selection Sort

---
+++ Sorting Algorithms/SelectionSort.js
@@ -0,0 +1,41 @@
+// unpotimized version
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    //   Assiging first element to be the lowest
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (lowest !== i) {
+      console.log(i, lowest);
+      // swapping
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+console.log(selectionSort([0, 2, 34, 22, 10, 19, 17]));
+
+// Optimized version
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    //   Assiging first element to be the lowest
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (lowest !== i) swap(arr, i, lowest);
+  }
+  return arr;
+}
+console.log(selectionSort([0, 2, 34, 22, 10, 19, 17]));
