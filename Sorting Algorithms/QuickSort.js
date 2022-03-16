a1b71cf9f715f0d04f747e0a3e700b20542b4482
Atharv <atharvbhandare2001@gmail.com>
2022-03-16 22:45:39 +0530

    quickSort implemented

--- Sorting Algorithms/QuickSort.js
+++ Sorting Algorithms/QuickSort.js
@@ -10,9 +10,22 @@ function pivot(arr, start = 0, end = arr.length - 1) {
     if (pivotValue > arr[i]) {
       pivotIndex++;
       swap(arr, pivotIndex, i);
-    }
+    } 
   }
   swap(arr, start, pivotIndex); /*swapping the starting element(the pivot) with the pivot index*/
   return pivotIndex;
 }
-console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
+
+function quickSort(arr, left=0, right=arr.length-1){
+    //base Case
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right) //returns the pivot index
+        //for sorting left part of the array from pivot value
+        quickSort(arr, left, pivotIndex-1); 
+        //for sorting right part of the array from pivot value
+        quickSort(arr, pivotIndex+1, right); 
+    }
+    return arr;
+}
+
+console.log(quickSort([100,12,1,5,88,191]));
