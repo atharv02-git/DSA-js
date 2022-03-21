553f194d962d81893771599cf8f48be32bc3e7e0
Atharv <atharvbhandare2001@gmail.com>
2022-03-21 20:44:35 +0530

    Adding instance methods

--- Data Structures/classes.js
+++ Data Structures/classes.js
@@ -4,12 +4,31 @@ class Student {
         this.firstName = firstName;
         this.lastName = lastName; 
         this.birthYear = birthYear;
+        this.scores = []
+    }
+    // adding instance methods
+    fullName() {
+        return `Your full Name is ${this.firstName}`
+    }
+    addScore(score){
+        this.scores.push(score);
+        return this.scores;
+    }
+    calculateAvg(){
+        let sum = this.scores.reduce((prevVal, currentVal) => {return prevVal + currentVal })
+        return sum/this.scores.length;
     }
 }
 
 let firstStudent = new Student('Atharv', 'Bhandare', 2001);
 let secondStudent = new Student('Virat', 'Kohli', 1988)
 
-firstStudent.birthYear = 2002;
-console.log(firstStudent)
-console.log(secondStudent)
+// // firstStudent.birthYear = 2002;
+// console.log(firstStudent)
+// // console.log(secondStudent)
+// console.log(firstStudent.fullName())
+
+console.log(firstStudent.addScore(45))
+console.log(firstStudent.addScore(65))
+console.log(firstStudent.addScore(95))
+console.log(firstStudent.calculateAvg())
