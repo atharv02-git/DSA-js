f5315d9fd0d3f8c5e205b6739fafefbc2ac56005
Atharv <atharvbhandare2001@gmail.com>
2022-03-23 12:48:31 +0530

    Initialization of singly linked list

---
+++ Data Structures/singlyLinkedList.js
@@ -0,0 +1,17 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+class singlyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    // defining push method
+    push(val){
+        
+    }
+}
