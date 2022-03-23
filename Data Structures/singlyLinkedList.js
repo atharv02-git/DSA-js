8bacd669ef82fb2b340d8acbe37db23597dfed08
Atharv <atharvbhandare2001@gmail.com>
2022-03-23 14:49:43 +0530

    Push function for singlyLinkedList implemented

--- Data Structures/singlyLinkedList.js
+++ Data Structures/singlyLinkedList.js
@@ -12,6 +12,20 @@ class singlyLinkedList{
     }
     // defining push method
     push(val){
-        
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length += 1;
+        return this; 
     }
 }
+
+const list = new singlyLinkedList()
+console.log(list.push('Ab'))
+console.log(list.push('vk'))
+console.log(list.head, list.tail)
