fd3d26f740233b014575c908b543398cc04a89e4
Atharv <atharvbhandare2001@gmail.com>
2022-03-23 19:18:58 +0530

    get and set  method  for singlyLinkedList implemented

--- Data Structures/singlyLinkedList.js
+++ Data Structures/singlyLinkedList.js
@@ -53,27 +53,44 @@ class singlyLinkedList {
     }
     return currentHead;
   }
-  unShift(val){
-    let newNode = new Node(val)
-    if(!this.head){
+  unShift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
-    }else {
+    } else {
       newNode.next = this.head;
       this.head = newNode;
     }
     this.length += 1;
     return list;
   }
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+    var counter = 0;
+    var current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+  set(index, val){
+    var foundNode = this.get(index);
+    if(foundNode){
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
 
 const list = new singlyLinkedList();
-// console.log(list.push("Ab"));
-// console.log(list.push("vk"));
-// console.log(list.push("glenn"));
-// console.log(list.pop());
-// console.log(list.shift());
-// console.log(list.shift());
-// console.log(list.shift());
-// console.log(list);
-console.log(list.unShift("Sachin"))
+console.log(list.push("Ab"));
+console.log(list.push("vk"));
+console.log(list.push("glenn"));
+console.log(list.set(2,'warner'))
+console.log(list.get(2))
+console.log(list)
