bc3b6c01c7624f5a6a839e3cffce515a0d0aee35
Atharv <atharvbhandare2001@gmail.com>
2022-03-23 15:40:51 +0530

    Pop function for singlyLinkedList implemented

--- Data Structures/singlyLinkedList.js
+++ Data Structures/singlyLinkedList.js
@@ -1,31 +1,52 @@
-class Node{
-    constructor(val){
-        this.val = val;
-        this.next = null;
-    }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
-class singlyLinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+class singlyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  // defining push method
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    // defining push method
-    push(val){
-        let newNode = new Node(val);
-        if(!this.head){
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length += 1;
-        return this; 
+    this.length += 1;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      //method for traversing until next is null
+      newTail = current;
+      current = current.next;
     }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      //special case
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
-const list = new singlyLinkedList()
-console.log(list.push('Ab'))
-console.log(list.push('vk'))
-console.log(list.head, list.tail)
+const list = new singlyLinkedList();
+console.log(list.push("Ab"));
+console.log(list.push("vk"));
+console.log(list.push("glenn"));
+console.log(list.pop());
+console.log(list)
